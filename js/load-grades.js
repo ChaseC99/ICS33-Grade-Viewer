f40f9ccf286cad28d359e8db78bc0d07bbc3c59d
Chase Carnaroli <chasecarnaroli@gmail.com>
2018-02-02 13:27:22 -0800

    delete excess code

--- js/load-grades.js
+++ js/load-grades.js
@@ -4,8 +4,6 @@ function sendrequest(func){
     // THIS CODE LOGS THE JSON OBJECT TO THE CONSOLE
     // Sets xlsm file source as the test file in our GitHub repo
     var url = "https://raw.githubusercontent.com/ChaseC99/ICS33-Grade-Viewer/master/gradesTestFile.xlsm";
-    //var url = "https://github.com/ChaseC99/ICS33-Grade-Viewer/blob/master/gradesTestFile.xlsm";
-    var grades_json;
 
     /* set up async GET request */
     var req = new XMLHttpRequest();
@@ -15,31 +13,14 @@ function sendrequest(func){
     // Tell request what to do with code once the request loads
     req.onload = function(e) {
         grades_json = loadgrades(req);
-        console.log(grades_json);
+        console.log(grades_json);   // Debugging code
         func(grades_json)
-        /*
-        var data = new Uint8Array(req.response);
-        var workbook = XLSX.read(data, {type:"array"});
-
-        // The first sheet is the one with the grades
-        // This gets its name
-        var grades_sheet_name = workbook.SheetNames[0];
-
-        // Get the grades sheet
-        var grades_sheet = workbook.Sheets[grades_sheet_name];
-
-        // Convert sheet to JSON and log it to console
-        grades_json = XLSX.utils.sheet_to_json(grades_sheet, {header:1});
-        console.log(grades_json);
-        */
     }
 
     // Send request
     req.send();
     req.responseType = "arraybuffer";
-    console.log(req);
-
-    return req;
+    console.log(req);   // Debugging code
 }
 
 function loadgrades(req){
