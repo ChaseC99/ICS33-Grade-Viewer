a0a0900b47923241ae760b24c36eeefe89fb0507
Tristan Jogminas <tristan.jogminas@gmail.com>
2018-02-18 14:07:51 -0800

    cookie new

--- js/gradeviewer.js
+++ js/gradeviewer.js
@@ -356,7 +356,7 @@ function loadHashGrades(){
     date = new Date();
     date.setTime(date.getTime() + 6048000000);
     document.cookie = hashIDinput + "; expires=" + date;
-    console.log(document.cookie)
+    console.log(decodeURIComponent(document.cookie))
     // Log hash id to console
     console.log('Hash Id: ' + hashIDinput);
 
