4e8b78038f29ad6f9d1678c6f290d2e3334dfc51
Chase Carnaroli <chasecarnaroli@gmail.com>
2018-02-23 13:23:19 -0800

    remove unneeded files

--- js/zip-reader.js
+++
@@ -1,6 +0,0 @@
-var url = 'https://www.ics.uci.edu/~pattis/ICS-33/ics33win18grades.zip';
-zip.createReader(new zip.HttpReader(url), function(zipReader){
-   zipReader.getEntries(function(entries){
-      alert(entries)
-   });
-}, onerror);
